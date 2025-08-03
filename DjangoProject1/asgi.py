2af3ef0b4ece0bb60e0a39e8f07a8128155d99e6
Gerard Nicolau <gerardnico21@gmail.com>
2025-08-04 00:08:53 +0200

    fix bugs

--- DjangoProject1/asgi.py
+++ DjangoProject1/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StifelioWeb.settings')
 
 application = get_asgi_application()
--- DjangoProject1/wsgi.py
+++ DjangoProject1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StifelioWeb.settings')
 
 application = get_wsgi_application()
